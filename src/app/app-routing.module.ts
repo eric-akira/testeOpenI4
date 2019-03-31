6d743889e3e0603ca95a79ba6235857b06ed6d93
Eric Akira <Eric Akira>
2019-03-31 15:16:15 -0300

    mais paginas, service e http module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'locations', loadChildren: './locations/locations.module#LocationsPageModule' },
+  { path: 'location-details', loadChildren: './location-details/location-details.module#LocationDetailsPageModule' },
   //{ path: 'search', loadChildren: './search/search.module#SearchPageModule' },
   //{ path: 'history', loadChildren: './history/history.module#HistoryPageModule' }
 ];
